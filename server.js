665835f2f5141883a91c541d5205622d33e9f7d4
Uzair Saleem <uzairsaleemdev@gmail.com>
2024-01-22 01:51:42 +0500

    Update BACKEND ADDING USERS POSTGRES

--- server.js
+++ server.js
@@ -2,93 +2,92 @@ const express = require("express");
 const bodyParser= require('body-parser');
 const cors= require('cors');
 const app = express();
-const database={
+const bcrypt = require('bcrypt');
+const db = require('knex')({
+    client:'pg',
+    connection:{
+        host:'127.0.0.1',
+        user:'postgres',
+        password:'test',
+        database:'smartbrain'
+    }
+});
+
+const database =  {
     users:[
         {
             name:'bais',
             email:'bais@gmail.com',
-            password:'mypass',
-            entries:0,
-            joined: new Date(),
-            id:'124'
-        },
-        {
-            name:'uzair',
-            email:'uzair@gmail.com',
             password:'abumusal',
-            entries:0,
-            joined: new Date(),
-            id:'123'
+            joined:'2001-11-11',
+            id:'1222',
+            entries:0
         }
     ]
 }
+
 app.use(cors());
 app.use(bodyParser.json());
 app.get('/',(req,res)=>{
-    res.json(database.users);
+   res.json(database.users);   
 })
 
 app.post('/signin',(req,res)=>{
-    if(req.body.email===database.users[0].email&&
+    if(req.body.email===database.users[0].email&&  
         req.body.password===database.users[0].password){
             res.json(database.users[0]);
         }
     else{
         res.status(400).json('error logging in!')
     }
-
-
 })
 
 
 app.post('/register',(req,res)=>{
     const {name, email, password}= req.body;
-    database.users.push({
-       name:name,
-       password:password,
-       email:email,
-       entries:0,
-       joined: new Date(),
-       id:'125' 
+    db('users')
+    .returning('*')    
+    .insert({
+        name:name,
+        email:email,
+        joined: new Date()
     })
-
-    res.json(database.users[database.users.length-1]);
+    .then(data=> res.json(data[0]))
+    .catch(err=>{
+        console.log(err,'error adding user')
+        res.status(400).json('error adding the user!')
+    })
+    
 
 
 })
 
 
 app.get('/profile/:id',(req,res)=>{
-
-
     const {id}= req.params;
-    let found = false;
-    database.users.forEach((user)=>{
-        if(user.id===id){
-            found = true;
-            return res.json(user);
+    db.select('*').from('users').where({id})
+    .then(user=>{
+        if(user.length){
+            res.json(user[0]);
+        }
+        else{
+            res.status(400).json('not found')
         }
     })
-    if(!found){
-        res.status(404).json('not found the user')
-    }
+    .catch(err=>res.status(400).json('error getting profile!!'))
 
 })
 
 app.put('/image',(req,res)=>{
 
     const {id}= req.body;
-    let found = false;
-    database.users.forEach((user)=>{
-        if(user.id===id){
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
+    db('users').where('id','=',id).increment('entries',1).returning('entries').then(entries=>{
+        res.json(entries);
     })
-    if(!found){
-        res.status(404).json('not found the user')   
-    }
+    .catch(err=>{
+        res.status(400).json('unable to update entries!!')
+    })
+  
 })
 // Routes that we have to add to over app
 // profile route GET request '/profile/:userId'
@@ -101,5 +100,5 @@ app.put('/image',(req,res)=>{
 
 app.listen(3000,()=>{
     console.log('app is listening on port 3000...')
-
+    
 })
